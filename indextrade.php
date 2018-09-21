44c566f7908cb19988c06a0428816506f534dcad
Dahrane Meriem <meriemdahrane@yahoo.fr>
2018-09-21 11:11:04 +0200

    Petit up des POST à revoir!!

--- indextrade.php
+++ indextrade.php
@@ -5,14 +5,8 @@ $auto = New Trade();
 // $auto->cherche($_POST['term']);
 
 
-
-$reponse = $auto->envoi($_POST);
-    // $reponse = $auto->envoi($_POST['nom1'], intval($_POST['num1']), $_POST['nom2'],
-    // intval($_POST['num2']), $_POST['nom3'], intval($_POST['num3']), $_POST['need1'],
-    // intval($_POST['numb1']), $_POST['need2'], intval($_POST['numb2']),$_POST['need3'],
-    // intval($_POST['numb3']));
+    $reponse = $auto->envoi($_POST['proposition'], ($_POST['besoin']), $_POST['id troc'],$_POST['quantite'], $_POST['id user'])
 
 
-echo ("reponse = $auto->toto");
 
  ?>
